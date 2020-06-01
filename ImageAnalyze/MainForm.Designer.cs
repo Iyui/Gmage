8729247984b688d640f5518d201bc2b2cdcdbbd3
Iyui <314754801@qq.com>
2020-06-01 16:06:30 +0800

    小改动

--- ImageAnalyze/MainForm.Designer.cs
+++ ImageAnalyze/MainForm.Designer.cs
@@ -548,7 +548,8 @@
             // panel1
             // 
             this.panel1.AllowDrop = true;
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.BackgroundImage = global::Gmage.Properties.Resources.Gmage;
